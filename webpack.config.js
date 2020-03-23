ac29457c1504d0eb920c792759d84bc9bf711ce1
ledamint <heresliker@gmail.com>
2020-03-23 20:29:17 +0300

    remove optional-chaining and add repository

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,6 @@ module.exports = {
                             [
                                 '@babel/plugin-transform-react-jsx',
                             ],
-                            '@babel/plugin-proposal-optional-chaining',
                         ],
                     },
                 },
