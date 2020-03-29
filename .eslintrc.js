38a04b2a021c52d665c4ec84d5842102a420b1d0
ledamint <heresliker@gmail.com>
2020-03-29 18:42:46 +0300

    init eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    env: {
+        node: true,
+        es6: true,
+    },
+};
