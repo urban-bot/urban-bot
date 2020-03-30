55ecc40df20197255675bd0a5ec5b77bf0e081ee
ledamint <ledamint@gmail.com>
2020-03-30 20:14:50 +0300

    move bot image message to reconciler

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,48 +2,19 @@ import React from 'react';
 import { useBotContext } from '../hooks';
 
 export function Image({ src, caption, inlineButtons }) {
-    const [messageData, setMessageData] = React.useState();
     const { userId, bot } = useBotContext();
 
-    React.useEffect(() => {
-        const params = {};
+    const params = {};
 
-        if (caption) {
-            params.caption = caption;
-        }
+    if (caption) {
+        params.caption = caption;
+    }
 
-        // FIXME doesn't work
-        if (inlineButtons) {
-            const { reply_markup } = inlineButtons.type(inlineButtons.props);
-            params.reply_markup = reply_markup;
-        }
+    // FIXME doesn't work
+    if (inlineButtons) {
+        const { reply_markup } = inlineButtons.type(inlineButtons.props);
+        params.reply_markup = reply_markup;
+    }
 
-        if (messageData === undefined) {
-            bot.sendPhoto(userId, src, params).then((res) => {
-                setMessageData(res);
-            });
-        } else {
-            const opts = {
-                chat_id: messageData.chat.id,
-                message_id: messageData.message_id,
-            };
-
-            const media = {
-                type: 'photo',
-                caption,
-                media: src,
-            };
-            bot.editMessageMedia(media, { ...params, ...opts });
-        }
-    }, [userId, inlineButtons, src, caption, bot, messageData]);
-
-    React.useEffect(() => {
-        return () => {
-            if (messageData) {
-                bot.deleteMessage(messageData.chat.id, messageData.message_id);
-            }
-        };
-    }, [messageData, bot]);
-
-    return null;
+    return <img src={src} bot={bot} userId={userId} params={params} />;
 }
