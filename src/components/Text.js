878e37b4a68a081e802c12f92a5cff3b66249a2d
ledamint <heresliker@gmail.com>
2020-03-30 21:44:25 +0300

    add Array example

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useBotContext } from '../hooks';
 
-export function Text({ children }) {
+export const Text = React.memo(function Text({ children }) {
     const { userId, bot } = useBotContext();
 
     return <message text={children} userId={userId} bot={bot} />;
-}
+});
