bac6446352eb3e4bb61c0a18b831a96c5151d0db
ledamint <heresliker@gmail.com>
2020-04-06 22:26:49 +0300

    add hooks

--- src/hooks.js
+++ src/hooks.js
@@ -9,7 +9,7 @@ export function useRouter() {
     return React.useContext(RouterContext);
 }
 
-export function useText(func) {
+function useSubscribe(func, event) {
     const { userId, bot } = useBotContext();
 
     React.useEffect(() => {
@@ -21,10 +21,70 @@ export function useText(func) {
             }
         }
 
-        bot.on('text', handler);
+        bot.on(event, handler);
 
         return () => {
-            bot.removeListener('text', handler);
+            bot.removeListener(event, handler);
         };
     }, [func, bot, userId]);
+}
+
+export function useMessage(func) {
+    useSubscribe(func, 'message');
+}
+
+export function useText(func) {
+    useSubscribe(func, 'text');
+}
+
+export function useSticker(func) {
+    useSubscribe(func, 'sticker');
+}
+
+export function useAnimation(func) {
+    useSubscribe(func, 'animation');
+}
+
+export function useAudio(func) {
+    useSubscribe(func, 'audio');
+}
+
+export function useContact(func) {
+    useSubscribe(func, 'contact');
+}
+
+export function useDocument(func) {
+    useSubscribe(func, 'document');
+}
+
+export function useInvoice(func) {
+    useSubscribe(func, 'invoice');
+}
+
+export function usePassportData(func) {
+    useSubscribe(func, 'passport_data');
+}
+
+export function useLocation(func) {
+    useSubscribe(func, 'location');
+}
+
+export function usePhoto(func) {
+    useSubscribe(func, 'photo');
+}
+
+export function usePoll(func) {
+    useSubscribe(func, 'poll');
+}
+
+export function useVideo(func) {
+    useSubscribe(func, 'video');
+}
+
+export function useVideoNote(func) {
+    useSubscribe(func, 'video_note');
+}
+
+export function useVoice(func) {
+    useSubscribe(func, 'voice');
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,50 @@ import { Text } from './components/Text';
 import { Image } from './components/Image';
 import { ButtonGroup, Button } from './components/ButtonGroup';
 import { Root } from './components/Root';
-import { useBotContext, useText, useRouter } from './hooks';
+import {
+    useBotContext,
+    useText,
+    useRouter,
+    useSticker,
+    useAnimation,
+    useMessage,
+    useAudio,
+    useContact,
+    useDocument,
+    useInvoice,
+    useLocation,
+    usePassportData,
+    usePhoto,
+    usePoll,
+    useVideo,
+    useVideoNote,
+    useVoice,
+} from './hooks';
 
-export { render, Router, Route, Button, useText, Text, Image, ButtonGroup, useBotContext, useRouter, Root };
+export {
+    render,
+    Router,
+    Route,
+    Button,
+    useText,
+    Text,
+    Image,
+    ButtonGroup,
+    useBotContext,
+    useRouter,
+    Root,
+    useSticker,
+    useAnimation,
+    useMessage,
+    useAudio,
+    useContact,
+    useDocument,
+    useInvoice,
+    useLocation,
+    usePassportData,
+    usePhoto,
+    usePoll,
+    useVideo,
+    useVideoNote,
+    useVoice,
+};
