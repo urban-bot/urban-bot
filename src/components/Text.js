6e2296c6ca2a6288b4709ee87e24319a9779ba2f
ledamint <heresliker@gmail.com>
2020-04-08 22:38:14 +0300

    add br

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useBotContext } from '../hooks';
+import { formatElementToHTML } from '../utils/formatElementToHTML';
 
 export function Text({ children, isNewMessageEveryRender: isNewMessageEveryRenderProp }) {
     const { userId, bot, isNewMessageEveryRender: isNewMessageEveryRenderContext } = useBotContext();
 
+    const text = formatElementToHTML(children);
+
     return (
         <text
-            text={children}
+            text={text}
             userId={userId}
             bot={bot}
             isNewMessageEveryRender={isNewMessageEveryRenderProp ?? isNewMessageEveryRenderContext}
+            params={{
+                parse_mode: 'HTML',
+            }}
         />
     );
 }
