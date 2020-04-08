e7623090f1696d27d6f251820a7f4761d5ada41c
ledamint <heresliker@gmail.com>
2020-04-08 23:26:46 +0300

    add fragment

--- src/utils/formatElementToHTML.test.js
+++ src/utils/formatElementToHTML.test.js
@@ -9,52 +9,36 @@ describe('formatText', () => {
 
     test('bold', () => {
         expect(formatElementToHTML(<b>text</b>)).toBe('<b>text</b>');
-        expect(formatElementToHTML([<b>text</b>, <b>text2</b>])).toBe('<b>text</b><b>text2</b>');
-        expect(formatElementToHTML([<b>text</b>, ' ', <b>text2</b>])).toBe('<b>text</b> <b>text2</b>');
     });
 
     test('italic', () => {
         expect(formatElementToHTML(<i>text</i>)).toBe('<i>text</i>');
-        expect(formatElementToHTML([<i>text</i>, <i>text2</i>])).toBe('<i>text</i><i>text2</i>');
     });
 
     test('underscore', () => {
         expect(formatElementToHTML(<u>text</u>)).toBe('<u>text</u>');
-        expect(formatElementToHTML([<u>text</u>, <u>text2</u>])).toBe('<u>text</u><u>text2</u>');
     });
 
     test('strikethrough', () => {
         expect(formatElementToHTML(<s>text</s>)).toBe('<s>text</s>');
-        expect(formatElementToHTML([<s>text</s>, <s>text2</s>])).toBe('<s>text</s><s>text2</s>');
     });
 
     test('code', () => {
         expect(formatElementToHTML(<code>text</code>)).toBe('<code>text</code>');
-        expect(formatElementToHTML([<code>text</code>, <code>text2</code>])).toBe(
-            '<code>text</code><code>text2</code>',
-        );
     });
 
     test('pre', () => {
         expect(formatElementToHTML(<pre>text</pre>)).toBe('<pre>text</pre>');
-        expect(formatElementToHTML([<pre>text</pre>, <pre>text2</pre>])).toBe('<pre>text</pre><pre>text2</pre>');
     });
 
     test('br', () => {
         expect(formatElementToHTML(<br />)).toBe('\n');
-        expect(formatElementToHTML(['hello', <br />, 'world'])).toBe('hello\nworld');
     });
 
     test('link', () => {
         expect(formatElementToHTML(<a href="http://www.example.com/">inline URL</a>)).toBe(
             '<a href="http://www.example.com/">inline URL</a>',
         );
-        expect(
-            formatElementToHTML([
-                <a href="http://www.example.com/">inline URL</a>,
-                <a href="http://www.example2.com/">inline URL2</a>,
-            ]),
-        ).toBe('<a href="http://www.example.com/">inline URL</a><a href="http://www.example2.com/">inline URL2</a>');
     });
 
     test('link user', () => {
@@ -65,6 +49,33 @@ describe('formatText', () => {
         expect(formatElementToHTML(<a userId="123456789">user URL</a>)).toBe(
             '<a href="tg://user?id=123456789">user URL</a>',
         );
+    });
+
+    test('react fragment', () => {
+        expect(
+            formatElementToHTML(
+                <>
+                    <b>text</b>
+                    <b>text2</b>
+                </>,
+            ),
+        ).toBe('<b>text</b><b>text2</b>');
+        expect(
+            formatElementToHTML(
+                <>
+                    <b>text</b>
+                    <>
+                        <b>text2</b> <b>text3</b>
+                    </>
+                    <b>text4</b>
+                </>,
+            ),
+        ).toBe('<b>text</b><b>text2</b> <b>text3</b><b>text4</b>');
+    });
+
+    test('flat structure', () => {
+        expect(formatElementToHTML([<b>text</b>, <b>text2</b>])).toBe('<b>text</b><b>text2</b>');
+        expect(formatElementToHTML([<b>text</b>, ' ', <b>text2</b>])).toBe('<b>text</b> <b>text2</b>');
     });
 
     test('deep structure', () => {
