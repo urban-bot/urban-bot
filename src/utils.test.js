ed008d7b2e3123056edbcae8678d0492f100e5d5
ledamint <ledamint@gmail.com>
2020-04-08 19:18:57 +0300

    check failed test

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,5 +1,5 @@
 import { shallowEqual } from './utils';
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(shallowEqual({ a: 1 }, { a: 1 })).toBe(true);
+    expect(shallowEqual({ a: 1 }, { a: 1 })).toBe(false);
 });
