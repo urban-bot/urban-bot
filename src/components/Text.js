142b9ce686bf1d59025ff5ce633362502dab6c8c
ledamint <heresliker@gmail.com>
2020-04-09 22:47:28 +0300

    add to params reply_markup if is needed

--- src/components/Text.js
+++ src/components/Text.js
@@ -24,17 +24,24 @@ export function Text({
     }
 
     const params = React.useMemo(() => {
-        return {
+        const params = {
             parse_mode: parseMode,
             disable_web_page_preview: disableWebPagePreview,
             disable_notification: disableNotification,
             reply_to_message_id: replyToMessageId,
-            reply_markup: {
+        };
+
+        if (forceReply !== undefined || selective !== undefined) {
+            const reply_markup = {
                 force_reply: forceReply,
                 selective: selective,
-            },
-        };
-    }, [parseMode, disableWebPagePreview, disableNotification, replyToMessageId, forceReply]);
+            };
+
+            params.reply_markup = reply_markup;
+        }
+
+        return params;
+    }, [parseMode, disableWebPagePreview, disableNotification, replyToMessageId, forceReply, selective]);
 
     return (
         <text
