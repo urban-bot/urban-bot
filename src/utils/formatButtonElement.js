b085af5debd86f35cf585f47d8bec38b1dc893d9
ledamint <heresliker@gmail.com>
2020-04-13 19:11:38 +0300

    update formatButtonElement

--- src/utils/formatButtonElement.js
+++ src/utils/formatButtonElement.js
@@ -1,9 +1,16 @@
+import { Button } from '../components/ButtonGroup';
+
 export function formatButtonElement(element) {
     const mockCallback = () => {};
     const elementArray = Array.isArray(element) ? element : [element];
 
     return elementArray.map((child) => {
-        const { children: text, onClick = mockCallback, ...other } = child.props;
-        return { text, onClick, ...other };
+        if (child.type !== Button) {
+            throw new Error('Please use only Button components inside ButtonGroup.');
+        }
+
+        const { children: text, onClick = mockCallback, id = String(Math.random()), ...other } = child.props;
+
+        return { text, onClick, id, ...other };
     });
 }
