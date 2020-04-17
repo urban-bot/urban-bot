5af28565cfca9a92989ad2430f8bdb964759da01
ledamint <ledamint@gmail.com>
2020-04-17 18:11:38 +0300

    move shallowEqual to ts

--- src/utils/shallowEqual.ts
+++ src/utils/shallowEqual.ts
@@ -1,4 +1,8 @@
-export function shallowEqual(obj1, obj2) {
+interface MapType {
+    [key: string]: any;
+}
+
+export function shallowEqual(obj1: MapType, obj2: MapType) {
     return Object.entries(obj1).every(([key1, value1]) => {
         return Object.is(value1, obj2[key1]);
     });
