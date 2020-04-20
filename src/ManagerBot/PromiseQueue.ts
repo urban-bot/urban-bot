c92e4d465c3ccb38dd6f3df56022d8fbc2090693
ledamint <heresliker@gmail.com>
2020-04-20 15:25:36 +0300

    move PromiseQueue to ts

--- src/ManagerBot/PromiseQueue.ts
+++ src/ManagerBot/PromiseQueue.ts
@@ -1,9 +1,13 @@
+type AnyFunction = (...args: any[]) => any;
+
 export class PromiseQueue {
+    last: Promise<any>;
+
     constructor() {
         this.last = Promise.resolve();
     }
 
-    next(callback) {
+    next(callback: AnyFunction) {
         this.last = this.last.then(callback);
         return this.last;
     }
