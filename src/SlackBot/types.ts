9003297ac97905c53dde09a04b14c32dab8c44cc
ledamint <ledamint@gmail.com>
2020-04-20 15:10:39 +0300

    describe slack meta type

--- src/SlackBot/types.ts
+++ src/SlackBot/types.ts
@@ -17,6 +17,7 @@ import {
     StaticSelect,
     UsersSelect,
 } from '@slack/types';
+import { WebAPICallResult } from '@slack/web-api';
 
 export type Respond = (message: any) => Promise<unknown>;
 
@@ -124,3 +125,123 @@ export type SlackCommandContext = {
 };
 
 export type SlackPayload = SlackActionContext | SlackMessageContext | SlackCommandContext;
+
+export type BotProfile = {
+    id: string;
+    deleted: boolean;
+    name: string;
+    app_id: string;
+    updated: number;
+    team_id: string;
+};
+
+export type SlackMessageMeta = WebAPICallResult & {
+    channel: string;
+    ts: string;
+    message: {
+        type: SlackMessageType;
+        text: string;
+        bot_id: string;
+        user: string;
+        ts: string;
+        team: string;
+    };
+    response_metadata?: WebAPICallResult['response_metadata'] & {
+        scopes?: SlackScope[];
+        acceptedScopes?: SlackScope[];
+    };
+    //     message: {
+    //
+    //         bot_profile: {
+    //             icons: [Object],
+    //         }
+    //     },
+    //     }
+    // }
+};
+
+export type SlackScope =
+    | 'admin'
+    | 'admin.apps:read'
+    | 'admin.apps:write'
+    | 'admin.conversations:read'
+    | 'admin.conversations:write'
+    | 'admin.invites:read'
+    | 'admin.invites:write'
+    | 'admin.teams:read'
+    | 'admin.teams:write'
+    | 'admin.users:read'
+    | 'admin.users:write'
+    | 'app_mentions:read'
+    | 'auditlogs:read'
+    | 'bot'
+    | 'channels:history'
+    | 'channels:join'
+    | 'channels:manage'
+    | 'channels:read'
+    | 'channels:write'
+    | 'chat:write'
+    | 'chat:write.customize'
+    | 'chat:write.public'
+    | 'chat:write:bot'
+    | 'chat:write:user'
+    | 'client'
+    | 'commands'
+    | 'conversations:history'
+    | 'conversations:read'
+    | 'conversations:write'
+    | 'dnd:read'
+    | 'dnd:write'
+    | 'dnd:write:user'
+    | 'emoji:read'
+    | 'files:read'
+    | 'files:write'
+    | 'files:write:user'
+    | 'groups:history'
+    | 'groups:read'
+    | 'groups:write'
+    | 'identify'
+    | 'identity.avatar'
+    | 'identity.avatar:read:user'
+    | 'identity.basic'
+    | 'identity.email'
+    | 'identity.email:read:user'
+    | 'identity.team'
+    | 'identity.team:read:user'
+    | 'identity:read:user'
+    | 'im:history'
+    | 'im:read'
+    | 'im:write'
+    | 'incoming-webhook'
+    | 'links:read'
+    | 'links:write'
+    | 'mpim:history'
+    | 'mpim:read'
+    | 'mpim:write'
+    | 'none'
+    | 'pins:read'
+    | 'pins:write'
+    | 'post'
+    | 'reactions:read'
+    | 'reactions:write'
+    | 'read'
+    | 'reminders:read'
+    | 'reminders:read:user'
+    | 'reminders:write'
+    | 'reminders:write:user'
+    | 'remote_files:read'
+    | 'remote_files:share'
+    | 'remote_files:write'
+    | 'search:read'
+    | 'stars:read'
+    | 'stars:write'
+    | 'team:read'
+    | 'tokens.basic'
+    | 'usergroups:read'
+    | 'usergroups:write'
+    | 'users.profile:read'
+    | 'users.profile:write'
+    | 'users.profile:write:user'
+    | 'users:read'
+    | 'users:read.email'
+    | 'users:write';
