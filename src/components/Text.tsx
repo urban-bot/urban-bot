a549b4da013ac1b9118340f89828f82023a45707
ledamint <ledamint@gmail.com>
2020-04-21 22:18:34 +0300

    move Text to ts

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import { useBotContext } from '../hooks/hooks';
 import { useFormatElement } from '../hooks/useFormatElement';
+import { UrbanMessageTextData } from '../types/Messages';
 
-export function Text(props) {
-    const {
-        children,
-        isNewMessageEveryRender: isNewMessageEveryRenderProp,
-        parseMode,
-        disableWebPagePreview,
-        disableNotification,
-        replyToMessageId,
-        forceReply,
-        ...otherProps
-    } = props;
+export type TextProps = Omit<UrbanMessageTextData, 'text'> & {
+    children: React.ElementType | string | number;
+    isNewMessageEveryRender?: boolean;
+};
 
+export function Text({
+    children,
+    isNewMessageEveryRender: isNewMessageEveryRenderProp,
+    parseMode,
+    disableWebPagePreview,
+    disableNotification,
+    replyToMessageId,
+    forceReply,
+    ...otherProps
+}: TextProps) {
     const { $$managerBot, isNewMessageEveryRender: isNewMessageEveryRenderContext, chat } = useBotContext();
 
     const [formattedText, finalParseMode] = useFormatElement(children, parseMode);
 
     return (
-        <text
+        <text1
             chat={chat}
             $$managerBot={$$managerBot}
             isNewMessageEveryRender={isNewMessageEveryRenderProp ?? isNewMessageEveryRenderContext}
