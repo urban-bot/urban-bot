cd454fe4e9827235516557fe21a5c2b6039a2e46
ledamint <ledamint@gmail.com>
2020-04-24 14:22:46 +0300

    update Queue

--- examples/Queue.tsx
+++ examples/Queue.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { Button, ButtonGroup, Text } from '../src';
 
 export function Queue() {
-    const [array, setArray] = React.useState(['0', '1', '2']);
-    const [index, setIndex] = React.useState(array.length);
+    const [queue, setQueue] = React.useState(['0', '1', '2']);
+    const [last, setLast] = React.useState(queue.length);
 
     const addLast = () => {
-        setArray([...array, String(index)]);
-        setIndex(index + 1);
+        setQueue([...queue, String(last)]);
+        setLast(last + 1);
     };
 
     const deleteFirst = () => {
-        const [_first, ...newArray] = array;
-        setArray(newArray);
+        const [_first, ...newArray] = queue;
+        setQueue(newArray);
     };
 
     return (
@@ -21,7 +21,7 @@ export function Queue() {
                 <Button onClick={addLast}>Push</Button>
                 <Button onClick={deleteFirst}>Delete first</Button>
             </ButtonGroup>
-            {array.map((value) => {
+            {queue.map((value) => {
                 return <Text key={value}>{value}</Text>;
             })}
         </>
