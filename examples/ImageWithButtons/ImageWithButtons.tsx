13bc495a0f9f8458057169fd6231d9b22b0709c6
ledamint <heresliker@gmail.com>
2020-04-24 15:06:59 +0300

    make normal paths

--- examples/ImageWithButtons/ImageWithButtons.tsx
+++ examples/ImageWithButtons/ImageWithButtons.tsx
@@ -4,14 +4,19 @@ import { Button, ButtonGroup, Image, useBotContext, useRouter, useText } from '.
 import React from 'react';
 import { UrbanSlackBot } from '../../src/SlackBot/UrbanSlackBot';
 
-// FIXME make normal paths
-const photoAN = fs.readFileSync(path.join(__dirname, '../../../examples/ImageWithButtons', 'an-24.jpg'));
+function getImageByURL() {
+    return 'https://upload.wikimedia.org/wikipedia/commons/3/32/Aeroflot_Tu-154B-2_CCCP-85396_ZRH_1982-6-20.png';
+}
+
+function getImageByFile() {
+    return fs.readFileSync(path.join(__dirname, 'an-24.jpg'));
+}
 
 export function ImageWithButtons() {
     const { bot } = useBotContext();
     const { navigate } = useRouter();
     const [title, setTitle] = React.useState('✈️');
-    const [isImageFromLink, setIsImageFromLink] = React.useState(true);
+    const [isImageFromURL, setIsImageFromURL] = React.useState(true);
 
     useText(({ payload }) => {
         setTitle(payload.text);
@@ -20,11 +25,7 @@ export function ImageWithButtons() {
     return (
         <>
             <Image
-                image={
-                    isImageFromLink
-                        ? 'https://upload.wikimedia.org/wikipedia/commons/3/32/Aeroflot_Tu-154B-2_CCCP-85396_ZRH_1982-6-20.png'
-                        : photoAN
-                }
+                image={isImageFromURL ? getImageByURL() : getImageByFile()}
                 title={bot.type !== UrbanSlackBot.TYPE ? <b>{title}</b> : title}
                 alt="planes"
                 buttons={
@@ -38,7 +39,7 @@ export function ImageWithButtons() {
                         </Button>
                         <Button
                             onClick={() => {
-                                setIsImageFromLink(!isImageFromLink);
+                                setIsImageFromURL(!isImageFromURL);
                             }}
                         >
                             Toggle picture
