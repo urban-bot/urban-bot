6b279a29de0f3ce12e4743ad4b77075e7c959ea7
ledamint <heresliker@gmail.com>
2020-04-26 14:32:45 +0300

    create getTypeByMimeType

--- src/SlackBot/UrbanSlackBot.ts
+++ src/SlackBot/UrbanSlackBot.ts
@@ -25,7 +25,7 @@ import {
     SLACK,
 } from './types';
 import { formatButtons, formatTitle } from './format';
-import { UrbanFile } from '../types';
+import { getTypeByMimeType } from './utils';
 
 const app = express();
 
@@ -115,7 +115,7 @@ export class UrbanSlackBot implements UrbanBot<UrbanNativeEventSlack, SlackMessa
         };
 
         if (ctx.files !== undefined) {
-            const files: UrbanFile[] = ctx.files.map((file) => {
+            const files = ctx.files.map((file) => {
                 return {
                     mimeType: file.mimetype,
                     width: file.original_w,
@@ -133,9 +133,9 @@ export class UrbanSlackBot implements UrbanBot<UrbanNativeEventSlack, SlackMessa
                     files,
                 },
             } as const;
-            const isAllImages = files.every(({ mimeType }) => mimeType?.split('/')[0] === 'image');
-            const isAllVideo = files.every(({ mimeType }) => mimeType?.split('/')[0] === 'video');
-            const isAllAudio = files.every(({ mimeType }) => mimeType?.split('/')[0] === 'audio');
+            const isAllImages = files.every(({ mimeType }) => getTypeByMimeType(mimeType) === 'image');
+            const isAllVideo = files.every(({ mimeType }) => getTypeByMimeType(mimeType) === 'video');
+            const isAllAudio = files.every(({ mimeType }) => getTypeByMimeType(mimeType) === 'audio');
 
             if (isAllImages) {
                 this.processUpdate({
