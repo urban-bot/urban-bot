498cf2f5e0f16f347274aa3a2ca725f87358eef5
ledamint <heresliker@gmail.com>
2020-05-01 22:20:21 +0300

    remove buttons

--- examples/Poll.tsx
+++ examples/Poll.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import { Button, ButtonGroup, Poll, Option } from '../src';
+import { Poll, Option } from '../src';
 
 export function PollExample() {
     return (
-        <Poll
-            question="How are you?"
-            buttons={
-                <ButtonGroup>
-                    <Button
-                        onClick={() => {
-                            console.log('poll');
-                        }}
-                    >
-                        Button
-                    </Button>
-                </ButtonGroup>
-            }
-        >
+        <Poll question="How are you?">
             <Option>Awesome</Option>
             <Option>Fine</Option>
             <Option>Ok</Option>
