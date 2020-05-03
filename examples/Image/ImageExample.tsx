653198257e538b0c06707b1a9ebff656a6a62773
ledamint <ledamint@gmail.com>
2020-05-03 22:09:00 +0300

    simplify title

--- examples/Image/ImageExample.tsx
+++ examples/Image/ImageExample.tsx
@@ -1,8 +1,7 @@
 import fs from 'fs';
 import path from 'path';
-import { Button, ButtonGroup, Image, useBotContext, useRouter, useText } from '../../src';
+import { Button, ButtonGroup, Image, useRouter, useText } from '../../src';
 import React from 'react';
-import { UrbanSlackBot } from 'urban-bot-slack';
 
 const imageByUrl =
     'https://upload.wikimedia.org/wikipedia/commons/3/32/Aeroflot_Tu-154B-2_CCCP-85396_ZRH_1982-6-20.png';
@@ -11,7 +10,6 @@ const imageByFile = fs.readFileSync(path.join(__dirname, 'an-24.jpg'));
 // const imageByFile = fs.createReadStream(path.join(__dirname, 'an-24.jpg'));
 
 export function ImageExample() {
-    const { bot } = useBotContext();
     const { navigate } = useRouter();
     const [title, setTitle] = React.useState('✈️');
     const [isImageFromURL, setIsImageFromURL] = React.useState(true);
@@ -23,7 +21,7 @@ export function ImageExample() {
     return (
         <Image
             file={isImageFromURL ? imageByUrl : imageByFile}
-            title={bot.type !== UrbanSlackBot.TYPE ? <b>{title}</b> : title}
+            title={title}
             alt="planes"
             buttons={
                 <ButtonGroup>
