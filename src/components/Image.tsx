2a93597190e643645186983ddd3ab1202d615107
German Smirnov <ledamint@gmail.com>
2020-05-06 23:59:28 +0300

    Fix format text (#53)
    * reformat formatText
    
    * rename useFormattedButtons

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useBotContext } from '../hooks/hooks';
-import { useFormattedText } from '../hooks/useFormattedText';
+import { formatText } from '../utils/formatText';
 import { UrbanMessageCommonData } from '../types/Messages';
 import { ButtonGroupProps } from './ButtonGroup';
-import { useFormattedButtons } from '../hooks/useFormattedButtons';
+import { getButtonsByButtonGroup } from '../utils/getButtonsByButtonGroup';
 import { UrbanFileFormat } from '../types';
 
 export type ImageProps = UrbanMessageCommonData & {
@@ -28,10 +28,16 @@ export function Image({
     forceReply,
     ...otherProps
 }: ImageProps) {
-    const { $$managerBot, isNewMessageEveryRender: isNewMessageEveryRenderContext, chat } = useBotContext();
+    const {
+        $$managerBot,
+        isNewMessageEveryRender: isNewMessageEveryRenderContext,
+        chat,
+        parseMode: parseModeContext,
+    } = useBotContext();
 
-    const [formattedTitle, finalParseMode] = useFormattedText(title, parseMode);
-    const formattedButtons = useFormattedButtons(buttonGroupElement);
+    const finalParseMode = parseMode ?? parseModeContext;
+    const formattedTitle = formatText(title, finalParseMode);
+    const formattedButtons = getButtonsByButtonGroup(buttonGroupElement);
 
     return (
         <urban-img
--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useBotContext } from '../hooks/hooks';
-import { useFormattedText } from '../hooks/useFormattedText';
+import { formatText } from '../utils/formatText';
 import { UrbanMessageCommonData } from '../types/Messages';
 import { ButtonGroupProps } from './ButtonGroup';
-import { useFormattedButtons } from '../hooks/useFormattedButtons';
+import { getButtonsByButtonGroup } from '../utils/getButtonsByButtonGroup';
 
 export type LocationProps = UrbanMessageCommonData & {
     latitude: number;
@@ -27,10 +27,16 @@ export function Location({
     replyToMessageId,
     ...otherProps
 }: LocationProps) {
-    const { $$managerBot, isNewMessageEveryRender: isNewMessageEveryRenderContext, chat } = useBotContext();
+    const {
+        $$managerBot,
+        isNewMessageEveryRender: isNewMessageEveryRenderContext,
+        chat,
+        parseMode: parseModeContext,
+    } = useBotContext();
 
-    const [formattedTitle, finalParseMode] = useFormattedText(title, parseMode);
-    const formattedButtons = useFormattedButtons(buttonGroupElement);
+    const finalParseMode = parseMode ?? parseModeContext;
+    const formattedTitle = formatText(title, finalParseMode);
+    const formattedButtons = getButtonsByButtonGroup(buttonGroupElement);
 
     return (
         <urban-location
