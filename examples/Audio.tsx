0bbbe717551b82da29df0acf1652ad0c838825e1
ledamint <heresliker@gmail.com>
2020-05-06 16:13:16 +0300

    remove buttons prop from examples

--- examples/Audio.tsx
+++ examples/Audio.tsx
@@ -1,34 +1,10 @@
 // import fs from 'fs';
 // import path from 'path';
 import React from 'react';
-import { Button, ButtonGroup, useText, Audio } from '../src';
+import { Audio } from '../src';
 
 // const audioByFile = fs.readFileSync(path.join(__dirname, 'audio.mp3'));
 
 export function AudioExample() {
-    const [author, setAuthor] = React.useState('Some author');
-
-    useText(({ text }) => {
-        setAuthor(text);
-    });
-
-    return (
-        <Audio
-            file={'urlToFile' /* || audioByFile */}
-            title="There is the song"
-            author={author}
-            name="Some name"
-            buttons={
-                <ButtonGroup>
-                    <Button
-                        onClick={() => {
-                            console.log('audio');
-                        }}
-                    >
-                        Button
-                    </Button>
-                </ButtonGroup>
-            }
-        />
-    );
+    return <Audio file={'urlToFile' /* || audioByFile */} title="There is the song" name="Some name" />;
 }
