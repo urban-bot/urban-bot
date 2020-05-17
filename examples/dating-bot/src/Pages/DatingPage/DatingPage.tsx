399de31fff3cc37537fea6cd276e9b9fde41956f
oTkPoBeHuE <xxoTkPoBeHuExx@gmail.com>
2020-05-17 20:32:08 +0300

    Dating bot: Add Dating page

---
+++ examples/dating-bot/src/Pages/DatingPage/DatingPage.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { useGetProfile } from '../../Common/api/requests/getProfile';
+import { UserProfile } from '../../Components/UserProfile/UserProfile';
+import { Button, ButtonGroup } from '@urban-bot/core';
+
+export function DatingPage() {
+    const [userId, setUserId] = useState(0);
+    const { data, error, loading } = useGetProfile(String(userId));
+
+    if (loading || error || !data) {
+        return null;
+    }
+
+    const handleClickPrevious = () => setUserId(userId > 0 ? userId - 1 : 0);
+    const handleClickNext = () => setUserId(userId + 1);
+
+    return (
+        <UserProfile
+            profile={data}
+            buttons={
+                <ButtonGroup>
+                    <Button onClick={handleClickPrevious}>{'<'}</Button>
+                    <Button onClick={handleClickPrevious}>{'❤'}</Button>
+                    <Button onClick={handleClickPrevious}>{'👎'}</Button>
+                    <Button onClick={handleClickNext}>{'>'}</Button>
+                </ButtonGroup>
+            }
+            isNewMessageEveryRender
+        />
+    );
+}
