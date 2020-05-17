e6d435b2a4559d81f0f7df7e176e17b310c8e434
oTkPoBeHuE <xxoTkPoBeHuExx@gmail.com>
2020-05-17 20:32:38 +0300

    Dating bot: Add UserProfile component

---
+++ examples/dating-bot/src/Components/UserProfile/UserProfile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image } from '@urban-bot/core';
+import { lang } from '../../Common/lang';
+import { ButtonGroupProps } from '@urban-bot/core/dist/components/ButtonGroup';
+import { Profile } from '../../Common/api/requests/types';
+
+type Props = {
+    profile: Profile;
+    buttons?: React.FunctionComponentElement<ButtonGroupProps>;
+    isNewMessageEveryRender?: boolean;
+};
+
+export function UserProfile({ profile, buttons, isNewMessageEveryRender }: Props) {
+    return (
+        <Image
+            file={profile.avatar}
+            title={
+                <>
+                    <b>{profile.name}</b>
+                    <br />
+                    Возраст: <i>{lang.AGE(profile.birthday)}</i> <br />
+                    <i>{profile.description}</i> <br />
+                </>
+            }
+            buttons={buttons}
+            isNewMessageEveryRender={isNewMessageEveryRender}
+        />
+    );
+}
