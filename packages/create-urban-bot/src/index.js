c7b55fdd9aaaab4b412515ab9fddd08e737223d0
ledamint <heresliker@gmail.com>
2020-05-25 16:05:43 +0300

    init create-urban-bot

---
+++ packages/create-urban-bot/src/index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import { execSync } from 'child_process';
+import path from 'path';
+
+const outDir = process.argv[2] || 'urban-bot-app';
+
+const commands = ['git clone https://github.com/urban-bot/urban-bot-starter.git ' + outDir, 'rimraf .git', 'npm i'];
+
+commands.forEach((command, i) => {
+    const dir = i > 0 ? outDir : '';
+    const cwd = path.resolve(process.cwd(), dir);
+
+    execSync(command, {
+        stdio: [0, 1, 2], // we need this so node will print the command output
+        cwd, // path to where you want to save the file
+    });
+});
