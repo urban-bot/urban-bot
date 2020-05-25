6b41f4e143a1053307a6ad0a56960b7b5680ac6f
German Smirnov <heresliker@gmail.com>
2020-05-25 16:40:16 +0300

    Create urban bot (#83)
    * init create-urban-bot
    
    * make ts
    
    * add npx to rimraf
    
    * add rename
    
    * bump version

---
+++ packages/create-urban-bot/src/index.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import { execSync } from 'child_process';
+import path from 'path';
+import fs from 'fs';
+
+const outDir = process.argv[2] || 'urban-bot-app';
+
+const commands = ['git clone https://github.com/urban-bot/urban-bot-starter.git ' + outDir, 'npx rimraf .git', 'npm i'];
+
+commands.forEach((command, i) => {
+    const dir = i > 0 ? outDir : '';
+    const cwd = path.resolve(process.cwd(), dir);
+
+    execSync(command, {
+        stdio: [0, 1, 2], // we need this so node will print the command output
+        cwd, // path to where you want to save the file
+    });
+});
+
+renameEnvFile();
+
+function renameEnvFile() {
+    const cwd = path.resolve(process.cwd(), outDir);
+    fs.renameSync(cwd + '/.env.example', cwd + '/.env');
+}
