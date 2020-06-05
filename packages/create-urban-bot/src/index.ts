9b278ba99ff7e5b3638946189e9e142ef8633a33
ledamint <heresliker@gmail.com>
2020-06-05 16:45:12 +0300

    add template

--- packages/create-urban-bot/src/index.ts
+++ packages/create-urban-bot/src/index.ts
@@ -5,7 +5,13 @@ import fs from 'fs';
 
 const outDir = process.argv[2] || 'urban-bot-app';
 
-const commands = ['git clone https://github.com/urban-bot/urban-bot-starter.git ' + outDir, 'npx rimraf .git', 'npm i'];
+const template = getTemplate();
+
+const commands = [
+    `git clone https://github.com/urban-bot/urban-bot-starter-${template}.git ${outDir}`,
+    'npx rimraf .git',
+    'npm i',
+];
 
 commands.forEach((command, i) => {
     const dir = i > 0 ? outDir : '';
@@ -22,4 +28,28 @@ renameEnvFile();
 function renameEnvFile() {
     const cwd = path.resolve(process.cwd(), outDir);
     fs.renameSync(cwd + '/.env.example', cwd + '/.env');
+}
+
+function getTemplate() {
+    const templateKeyIndex = process.argv.findIndex((arg) => {
+        return arg === '--template';
+    });
+
+    if (templateKeyIndex === -1) {
+        return 'typescript';
+    }
+
+    const templateShortName = process.argv[templateKeyIndex + 1];
+
+    switch (templateShortName) {
+        case 'js': {
+            return 'javascript';
+        }
+        case 'ts': {
+            return 'typescript';
+        }
+        default: {
+            throw new Error('Template is not found. Use "ts" or "js"');
+        }
+    }
 }
