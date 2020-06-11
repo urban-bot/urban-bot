6e2a794439cac3b82f370e68301cf850a54293bf
ledamint <ledamint@gmail.com>
2020-06-11 18:47:01 +0300

    init button columns

--- packages/urban-bot/src/components/ButtonGroup.tsx
+++ packages/urban-bot/src/components/ButtonGroup.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useBotContext, useAction } from '../hooks/hooks';
-import { formatButtonElement } from '../utils/formatButtonElement';
+import { formatButtonElement, FormattedButton } from '../utils/formatButtonElement';
 import { UrbanMessageCommonData } from '../types/Messages';
 import { OtherProps } from '../types/common';
 import { flatten } from 'array-flatten';
 import { formatMarkupLanguageElement } from '../utils/formatMarkupLanguageElement';
 import { getParseMode } from '../utils/getParseMode';
+import { groupFlatArray } from '../utils/groupFlatArray';
 
 export type ButtonGroupProps = UrbanMessageCommonData & {
     title?: React.ReactNode;
     isReplyButtons?: boolean;
     isNewMessageEveryRender?: boolean;
+    columns?: number;
     children: React.ReactElement<ButtonProps> | React.ReactElement<ButtonProps>[] | React.ReactElement<ButtonProps>[][];
 };
 
@@ -22,6 +24,7 @@ export function ButtonGroup({
     disableNotification,
     replyToMessageId,
     forceReply,
+    columns,
     isReplyButtons = false,
     ...otherProps
 }: ButtonGroupProps) {
@@ -33,7 +36,11 @@ export function ButtonGroup({
         bot,
     } = useBotContext();
 
-    const buttons = formatButtonElement(children);
+    let buttons = formatButtonElement(children);
+
+    if (typeof columns === 'number' && !Array.isArray(buttons[0])) {
+        buttons = groupFlatArray(buttons as FormattedButton[], columns);
+    }
 
     const finalParseMode = getParseMode(title, parseMode, parseModeContext, bot.defaultParseMode);
     const formattedTitle = formatMarkupLanguageElement(title, finalParseMode);
---
+++ packages/urban-bot/src/utils/groupFlatArray.test.ts
@@ -0,0 +1,40 @@
+import { groupFlatArray } from './groupFlatArray';
+
+describe('groupFlatArray', () => {
+    it("can't pass zero", () => {
+        expect(() => groupFlatArray([1, 2, 3], 0)).toThrowErrorMatchingSnapshot();
+    });
+
+    it("can't pass less than zero", () => {
+        expect(() => groupFlatArray([1, 2, 3], -1)).toThrowErrorMatchingSnapshot();
+    });
+
+    it('return empty array', () => {
+        expect(groupFlatArray([], 1)).toEqual([]);
+    });
+
+    it('return group array with one element', () => {
+        expect(groupFlatArray([1], 1)).toEqual([[1]]);
+        expect(groupFlatArray([1], 2)).toEqual([[1]]);
+    });
+
+    it('return group array with two elements', () => {
+        expect(groupFlatArray([1, 2], 1)).toEqual([[1], [2]]);
+        expect(groupFlatArray([1, 2], 2)).toEqual([[1, 2]]);
+    });
+
+    it('return group array with three elements', () => {
+        expect(groupFlatArray([1, 2, 3], 1)).toEqual([[1], [2], [3]]);
+        expect(groupFlatArray([1, 2, 3], 2)).toEqual([[1, 2], [3]]);
+        expect(groupFlatArray([1, 2, 3], 3)).toEqual([[1, 2, 3]]);
+    });
+
+    it('return group array with four elements', () => {
+        expect(groupFlatArray([1, 2, 3, 4], 2)).toEqual([
+            [1, 2],
+            [3, 4],
+        ]);
+        expect(groupFlatArray([1, 2, 3, 4], 3)).toEqual([[1, 2, 3], [4]]);
+        expect(groupFlatArray([1, 2, 3, 4], 5)).toEqual([[1, 2, 3, 4]]);
+    });
+});
