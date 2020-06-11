78d7ceba4e504b04115547469ba0acd24a72f726
German Smirnov <heresliker@gmail.com>
2020-06-11 19:11:17 +0300

    Todo list (#100)
    * init todo-list
    
    * move title
    
    * remove .eslintignore
    
    * init button columns
    
    * add doc and rename to maxColumns
    
    * run test

--- packages/urban-bot/src/components/ButtonGroup.tsx
+++ packages/urban-bot/src/components/ButtonGroup.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useBotContext, useAction } from '../hooks/hooks';
-import { formatButtonElement } from '../utils/formatButtonElement';
+import { formatButtonElement, FormattedButton } from '../utils/formatButtonElement';
 import { UrbanMessageCommonData } from '../types/Messages';
 import { OtherProps } from '../types/common';
 import { flatten } from 'array-flatten';
 import { formatMarkupLanguageElement } from '../utils/formatMarkupLanguageElement';
 import { getParseMode } from '../utils/getParseMode';
+import { groupFlatArray } from '../utils/groupFlatArray';
 
 export type ButtonGroupProps = UrbanMessageCommonData & {
     title?: React.ReactNode;
     isReplyButtons?: boolean;
     isNewMessageEveryRender?: boolean;
+    maxColumns?: number;
     children: React.ReactElement<ButtonProps> | React.ReactElement<ButtonProps>[] | React.ReactElement<ButtonProps>[][];
 };
 
@@ -22,6 +24,7 @@ export function ButtonGroup({
     disableNotification,
     replyToMessageId,
     forceReply,
+    maxColumns,
     isReplyButtons = false,
     ...otherProps
 }: ButtonGroupProps) {
@@ -33,7 +36,11 @@ export function ButtonGroup({
         bot,
     } = useBotContext();
 
-    const buttons = formatButtonElement(children);
+    let buttons = formatButtonElement(children);
+
+    if (typeof maxColumns === 'number' && !Array.isArray(buttons[0])) {
+        buttons = groupFlatArray(buttons as FormattedButton[], maxColumns);
+    }
 
     const finalParseMode = getParseMode(title, parseMode, parseModeContext, bot.defaultParseMode);
     const formattedTitle = formatMarkupLanguageElement(title, finalParseMode);
