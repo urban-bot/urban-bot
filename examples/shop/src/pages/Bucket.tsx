59dc6ba006e6821443f2820f033ee4acbda87190
ledamint <heresliker@gmail.com>
2020-06-15 19:12:16 +0300

    create BucketProvider

--- examples/shop/src/pages/Bucket.tsx
+++ examples/shop/src/pages/Bucket.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useBucket } from '../store/bucket';
+import { Text } from '@urban-bot/core';
 
 export function Bucket() {
-    // const { catalog } = useStore();
+    const { addedProducts } = useBucket();
 
-    return null;
+    return <Text>{addedProducts.size}</Text>;
 }
--- examples/shop/src/types.ts
+++ examples/shop/src/types.ts
@@ -1,4 +1,5 @@
 export type Product = {
+    id: number;
     name: string;
     images: string[];
 };
