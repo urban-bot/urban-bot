c64bb23e214c4beff1c7c8d655af3ea3d700dde4
ledamint <ledamint@gmail.com>
2020-06-15 19:12:16 +0300

    create BucketProvider

--- examples/shop/src/pages/Catalog.tsx
+++ examples/shop/src/pages/Catalog.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Image, ButtonGroup, Button, Text } from '@urban-bot/core';
 import { calculateCircularIndex } from '../utils';
-import { useProducts } from '../store/ProductsProvider';
+import { useProducts } from '../store/products';
+import { useBucket } from '../store/bucket';
 
 export function Catalog() {
     const [productIndex, setProductIndex] = useState(0);
     const [imageIndex, setImageIndex] = useState(0);
     const { products, fetchProducts } = useProducts();
+    const { addProduct, addedProducts } = useBucket();
 
     useEffect(() => {
         fetchProducts();
@@ -32,16 +34,23 @@ export function Catalog() {
         setImageIndex(calculateCircularIndex(imageIndex + 1, activeProduct.images.length));
     }
 
+    function addProductToBucket() {
+        addProduct(activeProduct);
+    }
+
+    const addedProduct = addedProducts.get(activeProduct.id);
+    const bucketCount = addedProduct ? addedProduct.count : 0;
+
     return (
         <Image
             title={activeProduct.name}
-            isNewMessageEveryRender={false}
             file={activeProduct.images[imageIndex]}
             buttons={
                 <ButtonGroup maxColumns={2}>
                     <Button onClick={previousProduct}>⬅️ prev</Button>
                     <Button onClick={nextProduct}>next ➡️</Button>
                     {activeProduct.images.length > 1 ? <Button onClick={nextImage}>🖼️</Button> : null}
+                    <Button onClick={addProductToBucket}>{`Add to bucket (${bucketCount})`}</Button>
                 </ButtonGroup>
             }
         />
---
+++ examples/shop/src/store/bucket.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, useContext } from 'react';
+import { Product } from '../types';
+
+type AddedProduct = Product & { count: number };
+
+export type AddedProducts = Map<number, AddedProduct>;
+
+type BucketContextType = {
+    addedProducts: AddedProducts;
+    addProduct: (product: Product) => void;
+    removeProduct: (product: Product) => void;
+};
+
+const BucketContext = createContext<BucketContextType>({} as BucketContextType);
+
+type BucketProviderProps = {
+    children: React.ReactNode;
+};
+
+export function BucketProvider({ children }: BucketProviderProps) {
+    const [addedProducts, setAddedProducts] = React.useState<AddedProducts>(new Map());
+
+    function addProduct(newProduct: Product) {
+        setAddedProducts((addedProducts) => {
+            if (addedProducts.has(newProduct.id)) {
+                const addedProduct = addedProducts.get(newProduct.id) as AddedProduct;
+
+                addedProduct.count += 1;
+            } else {
+                addedProducts.set(newProduct.id, { ...newProduct, count: 1 });
+            }
+
+            return new Map(addedProducts);
+        });
+    }
+
+    function removeProduct(deletedProduct: Product) {
+        if (addedProducts.has(deletedProduct.id)) {
+            const addedProduct = addedProducts.get(deletedProduct.id) as AddedProduct;
+
+            addedProduct.count -= 1;
+
+            if (addedProduct.count === 0) {
+                addedProducts.delete(deletedProduct.id);
+            }
+
+            return new Map(addedProducts);
+        }
+    }
+
+    return (
+        <BucketContext.Provider value={{ addedProducts, addProduct, removeProduct }}>{children}</BucketContext.Provider>
+    );
+}
+
+export const useBucket = () => useContext(BucketContext);
