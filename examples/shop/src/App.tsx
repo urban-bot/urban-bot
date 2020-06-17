710d9c6febff2e35da87f6aa8c5d935d6eef6f9d
ledamint <heresliker@gmail.com>
2020-06-17 17:33:49 +0300

    add firstName

--- examples/shop/src/App.tsx
+++ examples/shop/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Button, ButtonGroup, Route, Router } from '@urban-bot/core';
+import { Button, ButtonGroup, Route, Router, useBotContext } from '@urban-bot/core';
 import { Bucket } from './pages/Bucket';
 import { Catalog } from './pages/Catalog';
 import { ProductsProvider } from './store/products';
 import { BucketProvider } from './store/bucket';
 
 export function App() {
+    const { chat } = useBotContext();
+
     return (
         <ProductsProvider>
             <BucketProvider>
-                <ButtonGroup title={"Welcome to Shop! Type 'bucket' or 'catalog'."} isReplyButtons isResizedKeyboard>
+                <ButtonGroup
+                    title={`Welcome to the Shop ${chat.firstName ?? ''}! Type 'bucket' or 'catalog'.`}
+                    isReplyButtons
+                    isResizedKeyboard
+                >
                     <Button>bucket</Button>
                     <Button>catalog</Button>
                 </ButtonGroup>
