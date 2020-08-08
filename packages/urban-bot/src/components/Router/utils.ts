eab13a529ce2c5430086ce3cfedb9b333bcf326c
Пахотин Иван <xxoTkPoBeHuExx@gmail.com>
2020-08-08 22:07:55 +0300

    Add params to router (#124)
    * Add "path-parser" to package.json
    
    * Add Params to RouterContext type
    
    * Add parameters extraction to Router.tsx
    
    * Add Params to Router example
    
    * add example router params
    
    Co-authored-by: ledamint <heresliker@gmail.com>

--- packages/urban-bot/src/components/Router/utils.ts
+++ packages/urban-bot/src/components/Router/utils.ts
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RouteProps } from './Route';
+import { Path } from 'path-parser';
 
 // TODO get from common utils
 export const matchPattern = (path: string, pattern: string | RegExp) => {
@@ -11,9 +12,21 @@ export const matchPattern = (path: string, pattern: string | RegExp) => {
         return pattern.test(path);
     }
 
-    return pattern === path;
+    const pathInstance = new Path(pattern);
+
+    return pathInstance.test(path) !== null;
 };
 
 export const matchChild = (path: string) => (child: React.ReactElement<RouteProps>) => {
     return matchPattern(path, child.props.path);
 };
+
+export function getParams(path: string, pattern?: string | RegExp) {
+    if (typeof pattern !== 'string') {
+        return undefined;
+    }
+
+    const pathInstance = new Path(pattern);
+
+    return pathInstance.test(path) ?? undefined;
+}
